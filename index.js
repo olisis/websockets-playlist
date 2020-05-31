d1fda5631cb0ad967c7eb7a69a3ca19cafbd9edd
Oleg <olisis@example.com>
2020-05-31 12:18:41 +0200

    change config to run on hiroku

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ var socket = require('socket.io');
 
 // App setup
 var app = express();
-var server = app.listen(4000, function(){
-    console.log('listening for requests on port 4000,');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log('Server is up!');
 });
 
 // Static files
